6edf52159e4acf03cb9ad3622b5154bcd2d077d9
Miguel Pinto <up201806206@fe.up.pt>
2022-10-04 14:59:39 +0100

    added tests

--- test/pt/up/fe/els2022/AppTest.java
+++ test/pt/up/fe/els2022/AppTest.java
@@ -10,4 +10,21 @@ public class AppTest {
         App app = new App("test/pt/up/fe/els2022/languageParser/syntactic/greatTest.txt");
         app.run();
     }
+
+    @Test
+    public void readWrite(){
+
+    }
+    @Test
+    public void readSortWrite(){
+
+    }
+    @Test
+    public void readAddWrite(){
+
+    }
+    @Test
+    public void readRemoveWrite(){
+
+    }
 }
