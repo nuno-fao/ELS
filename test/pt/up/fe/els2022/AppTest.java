15bcd1a7aa6d9e4ee2282161c9cbdd5fd14406bc
Miguel Pinto <up201806206@fe.up.pt>
2022-10-04 22:26:33 +0100

    added tests

--- test/pt/up/fe/els2022/AppTest.java
+++ test/pt/up/fe/els2022/AppTest.java
@@ -13,18 +13,22 @@ public class AppTest {
 
     @Test
     public void readWrite(){
-
+        App app = new App("test/pt/up/fe/els2022/configFiles/testReadWrite.txt");
+        app.run();
     }
     @Test
     public void readSortWrite(){
-
+        App app = new App("test/pt/up/fe/els2022/configFiles/testReadSortWrite.txt");
+        app.run();
     }
     @Test
     public void readAddWrite(){
-
+        App app = new App("test/pt/up/fe/els2022/configFiles/testReadAddWrite.txt");
+        app.run();
     }
     @Test
     public void readRemoveWrite(){
-
+        App app = new App("test/pt/up/fe/els2022/configFiles/testReadRemoveWrite.txt");
+        app.run();
     }
 }
